d4da5e821084fae915eb4bda6eeb8034b86c1cc0
Davi Munhoz <davimunhoz1005@gmail.com>
2025-08-24 18:08:36 -0300

    fix(docker): exclude dockerfile and fix to use it local

--- db/connection.py
+++ db/connection.py
@@ -3,6 +3,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-db_url = f"postgresql+psycopg://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@localhost:{os.getenv('DB_PORT')}/{os.getenv('POSTGRES_DB')}"
-
-
+db_url = (f"postgresql+psycopg://"
+          f"{os.getenv('POSTGRES_USER')}:"
+          f"{os.getenv('POSTGRES_PASSWORD')}@"
+          f"localhost:"
+          f"{os.getenv('DB_PORT')}/"
+          f"{os.getenv('POSTGRES_DB')}"
+          )
