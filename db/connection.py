9daee55a0e4760247ada153ba587ed7d472da158
Davi Munhoz <davimunhoz1005@gmail.com>
2025-08-24 16:50:51 -0300

    fix(connetion): update to use the proper elements of the .env file

--- db/connection.py
+++ db/connection.py
@@ -3,5 +3,5 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-db_url = f"postgresql+psycopg://{os.getenv('USER_DB')}:{os.getenv('PASSWORD_DB')}@localhost:{os.getenv('DB_PORT')}/{os.getenv('NAME_DB')}"
+db_url = f"postgresql+psycopg://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@localhost:{os.getenv('DB_PORT')}/{os.getenv('NAME_DB')}"
 
