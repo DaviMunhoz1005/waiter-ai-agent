27b77e02b6e2575e1791269814a94b4ec0b0b870
Davi Munhoz <davimunhoz1005@gmail.com>
2025-08-24 17:22:00 -0300

    fix(requirements.txt): insert the requirements for docker build

--- db/connection.py
+++ db/connection.py
@@ -3,5 +3,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-db_url = f"postgresql+psycopg://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@localhost:{os.getenv('DB_PORT')}/{os.getenv('NAME_DB')}"
+db_url = f"postgresql+psycopg://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@localhost:{os.getenv('DB_PORT')}/{os.getenv('POSTGRES_DB')}"
+
 
